39f6a9733bff2cf7298136bfad0e5e23bbb8fe69
Khalid Noor <khalidnoor@Khalids-MBP.client.easlab.co.uk>
2023-07-10 16:18:31 +0100

    update

--- db/app.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tasks (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  description TEXT,
-  due_date DATE,
-  status ENUM('completed', 'not completed') DEFAULT 'not completed',
-  PRIMARY KEY (id)
-);
